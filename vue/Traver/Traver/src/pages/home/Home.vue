18c5c46b3f4f8b7f6c0394a017b5c49612188833
1191472467@qq.com <wang1386528@gmail.com>
2018-08-23 17:29:11 +0800

    vue css

--- vue/Traver/Traver/src/pages/home/Home.vue
+++ vue/Traver/Traver/src/pages/home/Home.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>this is home </div>
+  <home-header></home-header>
 </template>
 
 <script>
+import HomeHeader from './components/header'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
---
+++ vue/Traver/Traver/src/pages/home/components/header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-center"></div>
+    <div class="header-right">地点</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header
+  display : flex
+  background-color : #66ccff
+  line-height : .84rem
+  .header-left
+    float : left
+    color : #ffffff
+    width : .64rem
+  .header-center
+    flex : 1
+    background-color : #ffffff
+    border-radius : .1rem
+    margin-top : .12rem
+    margin-bottom : .12rem
+    line-height : 60rem
+  .header-right
+    width : 1.24rem
+    float : right
+    color : #ffffff
+    text-align : center
+</style>
