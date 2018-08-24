38afe588012002d1318d8fa0e8205add89142412
1191472467@qq.com <wang1386528@gmail.com>
2018-08-24 11:28:57 +0800

    vue header

--- vue/Traver/Traver/src/pages/home/components/header.vue
+++ vue/Traver/Traver/src/pages/home/components/header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-center"></div>
-    <div class="header-right">地点</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-center">
+      <span class="iconfont">&#xe632;</span>
+      请输入城市/景点
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -13,21 +21,28 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '~@@/variables.styl'
+
 .header
   display : flex
-  background-color : #66ccff
+  background-color : $bgColor
   line-height : .84rem
   .header-left
     float : left
     color : #ffffff
     width : .64rem
+    .back-icon
+      margin-left : .1rem
+      font-size : .18rem
   .header-center
     flex : 1
     background-color : #ffffff
     border-radius : .1rem
     margin-top : .12rem
-    margin-bottom : .12rem
-    line-height : 60rem
+    margin-bottom : .10rem
+    padding-left : .1rem
+    line-height : .62rem
+    color : #C0C0C0
   .header-right
     width : 1.24rem
     float : right
