f968282f330c5e969a5cf2aacabad8681c3834e3
1191472467@qq.com <wang1386528@gmail.com>
2018-08-24 15:56:10 +0800

    swiper

--- vue/Traver/Traver/src/main.js
+++ vue/Traver/Traver/src/main.js
@@ -1,14 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
-import './assets/styles/iconfont.css'
+import '@@/reset.css'
+import 'swiper/dist/css/swiper.css'
+import '@@/border.css'
+import '@@/iconfont.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 fastClick.attach(document.body)
 
 /* eslint-disable no-new */
---
+++ vue/Traver/Traver/src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="swiper-content">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg"/>
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="https://img1.qunarzz.com/piao/fusion/1808/fd/fddc5309111a0402.jpg_750x200_e7b93551.jpg"/>
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1805/44/70b819ab2b3c02.jpg_750x200_fe9147c8.jpg"/>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .swiper-content >>> .swiper-pagination-bullet-active
+    background : #ffffff
+  .swiper-content
+    overflow: hidden /**禁用滚动条**/
+    width: 100%
+    height: 0
+    padding-bottom: 26% /**宽高比例**/
+    background: #eee
+    .swiper-img
+      width : 100%
+</style>
