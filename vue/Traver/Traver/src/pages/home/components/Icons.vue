69c1a062c81f2791f3cf9aaf83a1c09165721467
1191472467@qq.com <wang1386528@gmail.com>
2018-08-27 15:23:19 +0800

    traver icon

---
+++ vue/Traver/Traver/src/pages/home/components/Icons.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="icon-container">
+    <div class="icon-content">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@@/variables.styl'
+
+  .icon-container
+    overflow : hidden
+    height : 0
+    width : 100%
+    padding-bottom : 50%
+    .icon-content
+      position : relative
+      float : left
+      height : 0
+      width : 25%
+      overflow : hidden
+      padding-bottom : 25%
+      .icon-img
+        position : absolute
+        top : 0
+        left : 0
+        right : 0
+        bottom : .44rem
+        box-sizing : border-box
+        padding : .1rem
+        .icon-img-content
+          display: block /**将img定义为块级元素,为了使margin有效**/
+          margin: 0 auto /**左右上下居中**/
+          height: 100%   /**图片完整显示 不拉伸**/
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color : $darkTextColor
+</style>
--- vue/Traver/Traver/src/pages/home/components/Swiper.vue
+++ vue/Traver/Traver/src/pages/home/components/Swiper.vue
@@ -2,14 +2,8 @@
   <div class="swiper-content">
     <swiper :options="swiperOption" ref="mySwiper">
       <!-- slides -->
-      <swiper-slide>
-        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg"/>
-      </swiper-slide>
-      <swiper-slide>
-        <img class="swiper-img" src="https://img1.qunarzz.com/piao/fusion/1808/fd/fddc5309111a0402.jpg_750x200_e7b93551.jpg"/>
-      </swiper-slide>
-      <swiper-slide>
-        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1805/44/70b819ab2b3c02.jpg_750x200_fe9147c8.jpg"/>
+      <swiper-slide v-for="item in list" :key="item.id">
+        <img class="swiper-img" :src="item.src"/>
       </swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -22,6 +16,16 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
+      list: [{
+        id: 1,
+        src: 'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
+      }, {
+        id: 2,
+        src: 'https://img1.qunarzz.com/piao/fusion/1808/fd/fddc5309111a0402.jpg_750x200_e7b93551.jpg'
+      }, {
+        id: 3,
+        src: 'http://img1.qunarzz.com/piao/fusion/1805/44/70b819ab2b3c02.jpg_750x200_fe9147c8.jpg'
+      }],
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
